2e1f6fe7d50f0b592fd5b535e1dddff560dc1d73
theskidster <theskidster@gmail.com>
2020-02-13 14:59:49 -0600

    new classes werent added in last commit, trying from master

---
+++ src/theskidster/xjge/entities/EntityTest.java
@@ -0,0 +1,70 @@
+package theskidster.xjge.entities;
+
+import org.joml.Vector3f;
+import static org.lwjgl.opengl.GL30.*;
+import org.lwjgl.system.MemoryStack;
+import theskidster.xjge.graphics.Graphics;
+import theskidster.xjge.shader.core.ShaderCore;
+import theskidster.xjge.util.ErrorUtil;
+
+/**
+ * @author J Hoffman
+ * Created: Feb 13, 2020
+ */
+
+/**
+ * A test entity that demonstrates the general structure of an object implementing {@link Entity}.
+ */
+public class EntityTest extends Entity {
+
+    private float angle;
+    
+    private Graphics g = new Graphics();
+    
+    public EntityTest(Vector3f position) {
+        super(position);
+        
+        try(MemoryStack stack = MemoryStack.stackPush()) {
+            g.vertices = stack.mallocFloat(18);
+            
+            //(vec3 position), (vec3 color)
+            g.vertices.put(-8).put(-8).put(0)   .put(1).put(0).put(0);
+            g.vertices .put(0) .put(8).put(0)   .put(0).put(1).put(0);
+            g.vertices .put(8).put(-8).put(0)   .put(0).put(0).put(1);
+            
+            g.vertices.flip();
+        }
+        
+        g.bindBuffers();
+        
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, (6 * Float.BYTES), 0);
+        glVertexAttribPointer(2, 3, GL_FLOAT, false, (6 * Float.BYTES), (3 * Float.BYTES));
+        
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(2);
+    }
+
+    @Override
+    public void update() {
+        angle += 1f;
+        
+        g.model.translation(position);
+        g.model.rotateY((float) Math.toRadians(-angle));
+    }
+
+    @Override
+    public void render() {
+        ShaderCore.use("default");
+        glBindVertexArray(g.vao);
+        
+        ShaderCore.setMat4("uModel", false, g.model);
+        ShaderCore.setInt("uType", 3);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        ErrorUtil.checkGLError();
+    }
+
+    @Override
+    protected void destroy() {}
+    
+}
