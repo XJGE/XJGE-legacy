c0754dedfcba77fcd2051a137bd0b849d82229e1
theskidster <theskidster@gmail.com>
2020-05-20 17:51:23 -0500

    updated version number

--- src/dev/theskidster/xjge/main/App.java
+++ src/dev/theskidster/xjge/main/App.java
@@ -82,7 +82,7 @@ public final class App {
     public static final int ALL_VIEWPORTS     = -1;
     public static final boolean DEBUG_ALLOWED = true; //TODO change this to false before building distributions.
     public static final String DOMAIN         = "xjge";
-    public static final String VERSION        = "1.2.0";
+    public static final String VERSION        = "1.2.3";
     public static final Matrix4f IDENTITY     = new Matrix4f();
     
     private static Viewport[] viewports = new Viewport[4];
